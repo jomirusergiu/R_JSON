427e9adb961d9fcd9c653b053c5ae8a557d7cae8
Admin <admin@130.41.168.192.in-addr.arpa>
2013-12-02 10:22:47 +0200

    Initial Commit

---
+++ R_JSON/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  R_JSON
+//
+//  Created by Admin on 12/2/13.
+//  Copyright (c) 2013 RocKK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
